25253d13084cfdce517e54a4d814d39e2a622be6
dohyeonj <jdh7693@naver.com>
2021-05-19 22:50:07 +0900

    펫 상세정보 디자인완료

--- src/main/java/com/naekkonyang/controller/PetController.java
+++ src/main/java/com/naekkonyang/controller/PetController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
